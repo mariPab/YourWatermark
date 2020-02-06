79f4aac934c468e8b839ca4e3bd1ba9fc662f72a
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-06 11:36:53 +0100

    Create basics of addImageWatermarkToImage function

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
         alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
         alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
     };
-    
+
     image.print(font, 10, 10, textData, image.getWidth(), image.getHeight());
     image.quality(100).write(outputFile);
 };
 
+const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+
+    image.composite(watermark, 0, 0, {
+        mode: Jimp.BLEND_SOURCE_OVER,
+        opacitySource: 0.5,
+    });
+    image.quality(100).write(outputFile);
+};
+
 addTextWatermarkToImage('./test.png', './test-with-watermark.jpg', 'Hello World');
+addImageWatermarkToImage('./test.png', './test-with-watermarkImage.jpg', './logo.png');
