5338ab897e4cd319c61aaa1639c956ca56ae92a9
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-06 11:42:26 +0100

    Center watermark image

--- app.js
+++ app.js
@@ -16,13 +16,15 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
 const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
     const image = await Jimp.read(inputFile);
     const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-    image.composite(watermark, 0, 0, {
+    image.composite(watermark, x, y, {
         mode: Jimp.BLEND_SOURCE_OVER,
         opacitySource: 0.5,
     });
     image.quality(100).write(outputFile);
 };
 
-addTextWatermarkToImage('./test.png', './test-with-watermark.jpg', 'Hello World');
+// addTextWatermarkToImage('./test.png', './test-with-watermark.jpg', 'Hello World');
 addImageWatermarkToImage('./test.png', './test-with-watermarkImage.jpg', './logo.png');
